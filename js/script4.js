fbc56eb0256638cdd02dc2678568372ab6bb7f2a
Harshit <harshit4762@gmail.com>
2024-04-14 03:17:04 +0530

    Added features to access and manage Local Storage (Add and Remove) in the Shopping List Project.

--- js/script4.js
+++ js/script4.js
@@ -4,38 +4,85 @@ const itemList = document.getElementById('item-list');
 const clrBtn = document.getElementById('clear');
 const filter = document.getElementById('filter');
 
-function addItem(e) {
+function displayItems() {
+  const itemsFromStorage = getItemFromStorage();
+  itemsFromStorage.forEach((item) => addItemToDOM(item));
+  checkUI();
+}
+
+function submitItem(e) {
   e.preventDefault();
   const newItem = itemInput.value;
   if (newItem === '') {
     alert('Please add item name.');
   }
+
+  addItemToDOM(newItem);
+  addItemToStorage(newItem);
+
+  itemInput.value = '';
+  checkUI();
+}
+
+function addItemToDOM(item) {
   const li = document.createElement('li');
   const button = document.createElement('button');
   button.className = 'remove-item btn-link text-red';
   const icon = document.createElement('i');
   icon.className = 'fa-solid fa-xmark';
   button.appendChild(icon);
-  li.appendChild(document.createTextNode(newItem));
+  li.appendChild(document.createTextNode(item));
   li.appendChild(button);
   itemList.appendChild(li);
-  itemInput.value = '';
-  checkUI();
 }
 
-function removeItem(e) {
-  if (e.target.parentElement.classList.contains('remove-item')) {
-    if (confirm('Are you sure?')) {
-      e.target.parentElement.parentElement.remove();
-      checkUI();
-    }
+function addItemToStorage(item) {
+  const itemsFromStorage = getItemFromStorage();
+
+  itemsFromStorage.push(item);
+
+  localStorage.setItem('items', JSON.stringify(itemsFromStorage));
+}
+
+function getItemFromStorage(item) {
+  let itemsFromStorage;
+  if (localStorage.getItem('items') === null) {
+    itemsFromStorage = [];
+  } else {
+    itemsFromStorage = JSON.parse(localStorage.getItem('items'));
   }
+  return itemsFromStorage;
+}
+
+function onClickItem(e) {
+  if (e.target.parentElement.classList.contains('remove-item')) {
+    removeItem(e.target.parentElement.parentElement);
+  }
+}
+
+function removeItem(item) {
+  if (confirm('Are you sure?')) {
+    item.remove();
+
+    removeItemFromStorage(item.textContent);
+    checkUI();
+  }
+}
+
+function removeItemFromStorage(item) {
+  let itemsFromStorage = getItemFromStorage();
+
+  itemsFromStorage = itemsFromStorage.filter((i) => i !== item);
+  localStorage.setItem('items', JSON.stringify(itemsFromStorage));
 }
 
 function removeAll(e) {
   while (itemList.firstChild) {
     itemList.firstChild.remove();
   }
+
+  localStorage.removeItem('items');
+
   checkUI();
 }
 
@@ -63,9 +110,15 @@ function checkUI() {
   }
 }
 
-itemForm.addEventListener('submit', addItem);
-itemForm.addEventListener('submit', addItem);
-itemList.addEventListener('click', removeItem);
-clrBtn.addEventListener('click', removeAll);
-filter.addEventListener('input', filterItems);
-checkUI();
+//Initialize App
+function init() {
+  itemForm.addEventListener('submit', submitItem);
+  itemList.addEventListener('click', onClickItem);
+  clrBtn.addEventListener('click', removeAll);
+  filter.addEventListener('input', filterItems);
+  document.addEventListener('DOMContentLoaded', displayItems);
+
+  checkUI();
+}
+
+init();
